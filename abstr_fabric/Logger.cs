e103d1e3fa0cff54afeeb4b6d984d76aaa719a77
ItzMeWoltik <itzmewoltik@gmail.com>
2024-03-05 00:23:08 +0200

    аеа, ну тіпа 1 коміт

---
+++ abstr_fabric/Logger.cs
@@ -0,0 +1,60 @@
+﻿public interface ILogger
+{
+    void Log(string message, LogLevel level);
+}
+
+public class FormattedLogger : ILogger
+{
+    public void Log(string message, LogLevel level)
+    {
+        string formattedMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}] {message}";
+        Console.WriteLine(formattedMessage);
+    }
+}
+
+public class ColoredLogger : ILogger
+{
+    private static Dictionary<LogLevel, ConsoleColor> colors = new Dictionary<LogLevel, ConsoleColor>
+    {
+        { LogLevel.Debug, ConsoleColor.Gray },
+        { LogLevel.Info, ConsoleColor.Green },
+        { LogLevel.Error, ConsoleColor.Red }
+    };
+
+    public void Log(string message, LogLevel level)
+    {
+        ConsoleColor originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = colors[level];
+        Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}] {message}");
+        Console.ForegroundColor = originalColor;
+    }
+}
+
+public enum LoggerType
+{
+    Formatted,
+    Colored
+}
+
+public static class LoggerFactory
+{
+    public static ILogger CreateLogger(LoggerType type)
+    {
+        switch (type)
+        {
+            case LoggerType.Formatted:
+                return new FormattedLogger();
+            case LoggerType.Colored:
+                return new ColoredLogger();
+            default:
+                throw new ArgumentException("Invalid logger type");
+        }
+    }
+}
+
+public enum LogLevel
+{
+    Debug,
+    Info,
+    Error
+}
---
+++ abstr_fabric/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        ILogger logger = LoggerFactory.CreateLogger(LoggerType.Formatted);
+        logger.Log("This is a debug message", LogLevel.Debug);
+        logger.Log("This is an info message", LogLevel.Info);
+        logger.Log("This is an error message", LogLevel.Error);
+
+        logger = LoggerFactory.CreateLogger(LoggerType.Colored);
+        logger.Log("This is a debug message", LogLevel.Debug);
+        logger.Log("This is an info message", LogLevel.Info);
+        logger.Log("This is an error message", LogLevel.Error);
+
+        Console.ReadLine();
+    }
+}
